67a004199b9a50a4ac396c82b5562f8aa6ce5e26
shimizu.kenji <shimizu.kenji@noncore.net>
2025-07-13 17:50:03 +0900

    feat: layout calendar view

--- src/main/kotlin/net/noncore/reportify/ui/windows/main/App.kt
+++ src/main/kotlin/net/noncore/reportify/ui/windows/main/App.kt
@@ -1,12 +1,16 @@
 package net.noncore.reportify.ui.windows.main
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import net.noncore.reportify.ui.components.CalendarView
 
 @Composable
@@ -17,9 +21,13 @@ fun App() {
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background,
         ) {
-            CalendarView(
-                modifier = Modifier.fillMaxWidth(),
-            )
+            Column(Modifier.padding(8.dp)) {
+                Card(
+                    modifier = Modifier.width(240.dp),
+                ) {
+                    CalendarView()
+                }
+            }
         }
     }
 }
