e681dabd0dc02997e4620ac1046c3649934e22f6
shimizu.kenji <shimizu.kenji@noncore.net>
2025-07-13 21:21:12 +0900

    chore: プロジェクトの構成を大幅に変更し、KotlinからTypeScriptおよびElectronに移行

--- src/main/kotlin/net/noncore/reportify/Main.kt
+++
@@ -1,9 +0,0 @@
-package net.noncore.reportify
-
-import androidx.compose.ui.window.application
-import net.noncore.reportify.ui.windows.main.MainWindow
-
-fun main() =
-    application {
-        MainWindow(this)
-    }
